163188529ae3a99e6dcffa0bd006aa7d69da87c2
PTOM76 <ptms76@outlook.jp>
2022-06-30 17:59:38 +0900

    port to 1.19

--- common/src/main/java/ml/pkom/uncraftingtable/Utils.java
+++ common/src/main/java/ml/pkom/uncraftingtable/Utils.java
@@ -1,8 +1,6 @@
 package ml.pkom.uncraftingtable;
 
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
-import net.minecraft.text.TranslatableText;
 
 public class Utils {
     public static Text translatableText(String key) {
