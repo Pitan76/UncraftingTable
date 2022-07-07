e595dea8523729e5d157ff662d270889f3f089b2
PTOM76 <ptms76@outlook.jp>
2022-07-07 16:32:45 +0900

    Fixed server crush

--- common/src/main/java/ml/pkom/uncraftingtable/Utils.java
+++ common/src/main/java/ml/pkom/uncraftingtable/Utils.java
@@ -1,13 +1,15 @@
 package ml.pkom.uncraftingtable;
 
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
 public class Utils {
-    public static Text translatableText(String key) {
-        return Text.translatable(key);
+    public static TranslatableText translatableText(String key) {
+        return new TranslatableText(key);
     }
 
     public static Text text(String string) {
-        return Text.literal(string);
+        return new LiteralText(string);
     }
 }
