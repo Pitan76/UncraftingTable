21a96013706b631063545267452bdc617c141808
PTOM76 <58260965+PTOM76@users.noreply.github.com>
2024-09-03 13:59:29 +0900

    neoforge

--- neoforge/src/main/java/net/pitan76/uncraftingtable/neoforge/UncraftingTableNeoForge.java
+++ neoforge/src/main/java/net/pitan76/uncraftingtable/neoforge/UncraftingTableNeoForge.java
@@ -4,9 +4,10 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.fml.loading.FMLPaths;
+import net.neoforged.neoforge.client.event.RegisterMenuScreensEvent;
 import net.pitan76.uncraftingtable.Config;
 import net.pitan76.uncraftingtable.UncraftingTable;
-import net.pitan76.uncraftingtable.neoforge.client.UncraftingTableNeoForgeClient;
+import net.pitan76.uncraftingtable.client.UncraftingScreen;
 
 @Mod(UncraftingTable.MOD_ID)
 public class UncraftingTableNeoForge {
@@ -18,12 +19,12 @@ public class UncraftingTableNeoForge {
             throw new IllegalStateException("modEventBus is null");
 
         UncraftingTable.init();
-        modEventBus.addListener(UncraftingTableNeoForgeClient::clientInit);
+
+        //modEventBus.addListener(UncraftingTableNeoForgeClient::clientInit);
+        modEventBus.addListener(this::registerScreens);
     }
 
-    /*
     private void registerScreens(RegisterMenuScreensEvent event) {
         event.register(UncraftingTable.UNCRAFTING_TABLE_MENU.get(), UncraftingScreen::new);
     }
-    */
 }
