33ac6b6256f6c8a8c0fd608148d1dfde309393bf
PTOM76 <ptms76@outlook.jp>
2025-06-01 12:20:20 +0900

    update loom version to 1.6-SNAPSHOT; add support for game versions 1.21.4 and 1.21.5

--- fabric/src/main/java/net/pitan76/uncraftingtable/fabric/UncraftingTableFabric.java
+++ fabric/src/main/java/net/pitan76/uncraftingtable/fabric/UncraftingTableFabric.java
@@ -1,18 +1,14 @@
 package net.pitan76.uncraftingtable.fabric;
 
-import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 import net.pitan76.uncraftingtable.Config;
 import net.pitan76.uncraftingtable.UncraftingTable;
-import net.pitan76.uncraftingtable.client.UncraftingTableClient;
 
 public class UncraftingTableFabric implements ModInitializer {
     @Override
     public void onInitialize() {
         Config.init(FabricLoader.getInstance().getConfigDir().toFile());
         new UncraftingTable();
-        if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT)
-            UncraftingTableClient.init();
     }
 }
---
+++ fabric/src/main/java/net/pitan76/uncraftingtable/fabric/client/UncraftingTableFabricClient.java
@@ -0,0 +1,14 @@
+package net.pitan76.uncraftingtable.fabric.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.pitan76.uncraftingtable.Config;
+import net.pitan76.uncraftingtable.client.UncraftingTableClient;
+
+public class UncraftingTableFabricClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        Config.init(FabricLoader.getInstance().getConfigDir().toFile());
+        UncraftingTableClient.init();
+    }
+}
